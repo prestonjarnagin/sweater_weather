2b7dd74b7c6ef7c4acd9e7183cd68745e99fd923
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-02 19:38:32 -0700

    Write geolocation service spec and add cassette

---
+++ spec/services/geolocation_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe GeolocationService do
+
+  let(:subject) { GeolocationService.new('denver,co') }
+
+  it 'exists' do
+    VCR.use_cassette("google_geolocation_service") do
+      expect(subject).to be_a(GeolocationService)
+    end
+  end
+
+  it 'returns coordinates' do
+    VCR.use_cassette("google_geolocation_service") do
+      expect(subject.coordinates[:lat].class).to be(Float)
+      expect(subject.coordinates[:lng].class).to be(Float)
+    end
+  end
+end
