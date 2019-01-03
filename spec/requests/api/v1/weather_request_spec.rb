1f1203219dc379cc2861f837494141e4e010924c
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-03 15:30:28 -0700

    Imporve testing

--- spec/requests/api/v1/weather_request_spec.rb
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Forecast API' do
+
   it 'GET /api/v1/forecast?location=denver,co' do
-    location = 'denver,co'
-    get "/api/v1/forecast?location=#{location}"
-    expect(response).to be_successful
+    VCR.use_cassette("google_geolocation_service") do
+      VCR.use_cassette("darksky_service") do
+        location = 'denver,co'
+
+        get "/api/v1/forecast?location=#{location}"
+        expect(response).to be_successful
+
+        data = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+        require "pry"; binding.pry
+        expect(data.keys).to include(:current)
+        expect(data.keys).to include(:seven_day)
+        expect(data.keys).to include(:hourly)
+      end
+    end
   end
 end
