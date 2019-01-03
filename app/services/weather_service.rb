9fa3edd112c2902a086bff8890b85181593a8473
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-03 15:34:11 -0700

    Finish WeatherService

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -5,6 +5,19 @@ class WeatherService
     @lng = lng
   end
 
-  
+  def get_json
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
+  def response
+    conn.get
+  end
+
+  def conn
+    Faraday.new("https://api.darksky.net/forecast/#{key}/#{@lat},#{@lng}")
+  end
+
+  def key
+    ENV['DARKSKY_API_KEY']
+  end
 end
