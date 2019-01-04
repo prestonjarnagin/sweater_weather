e565c75106e3b3e4820535bf1ef4f47d27856931
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-04 11:55:47 -0700

    Refactory user request spec

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -2,55 +2,48 @@ require 'rails_helper'
 
 RSpec.describe 'User API' do
 
-  it 'POST /api/v1/users succeeds' do
-    expect(User.all.count).to eq(0)
+  describe 'POST' do
+    describe '/api/v1/users' do
+      let(:payload) {
+        '{
+          "email": "whatever@example.com",
+          "password": "password",
+          "password_confirmation": "password"
+        }'
+      }
 
-    payload = {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
-    }
-    post "/api/v1/users", params: payload
+      it 'Succeeds with correct params' do
+        expect(User.all.count).to eq(0)
+        post "/api/v1/users", params: payload, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+        # post "/api/v1/users", payload
+        expect(response).to be_successful
+        data = JSON.parse(response.body, symbolize_names: true)
+        expect(data.keys).to include(:api_key)
+        expect(User.all.count).to eq(1)
+      end
 
-    expect(response).to be_successful
-    data = JSON.parse(response.body, symbolize_names: true)
+      it 'Fails with repeated password' do
+        expect(User.all.count).to eq(0)
+        payload['password_confirmation'] = 'different password'
+        post "/api/v1/users", params: payload
+        expect(response).to have_http_status(400)
+      end
 
-    expect(data.keys).to include(:api_key)
-    expect(User.all.count).to eq(1)
-  end
+      it 'Fails when email is already in use' do
+        expect(User.all.count).to eq(0)
+        post "/api/v1/users", params: payload
+        post "/api/v1/users", params: payload
+        expect(response).to have_http_status(400)
+        expect(User.all.count).to eq(1)
+      end
+    end
 
-  it 'POST /api/v1/users fails with repeated password' do
-    expect(User.all.count).to eq(0)
-
-    payload = {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "different password"
-    }
-    post "/api/v1/users", params: payload
-
-    expect(response).to have_http_status(400)
-  end
-  it 'POST /api/v1/users fails with repeated email' do
-    expect(User.all.count).to eq(0)
-
-    payload = {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
-    }
-    post "/api/v1/users", params: payload
-
-    payload = {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
-    }
-    post "/api/v1/users", params: payload
-    expect(response).to have_http_status(400)
-    expect(User.all.count).to eq(1)
-
-
-
+    describe '/api/v1/sessions' do
+      it 'Succeeds when correct' do
+        user = create(:user)
+        require "pry"; binding.pry
+        post
+      end
+    end
   end
 end
