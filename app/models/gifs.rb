2b27065fbaca7d7af8c4678c7c84be17598967c6
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-04 09:50:31 -0700

    Complete first working request

--- app/models/gifs.rb
+++ app/models/gifs.rb
@@ -7,12 +7,12 @@ class Gifs
   def weather
     @seven_day_weather.each do |weather_instance|
       require "pry"; binding.pry
-      response(weather_instance.summary)
+      get_json(weather_instance.summary)
     end
   end
 
   def conn
-    Faraday.new(url: "http://api.giphy.com/v1") do |f|
+    Faraday.new(url: "http://api.giphy.com") do |f|
       f.adapter  Faraday.default_adapter
     end
   end
@@ -20,10 +20,14 @@ class Gifs
 
   def response(search_query)
     conn.get do |req|
-      req.url "/search"
+      req.url "/v1/gifs/search"
       req.params['api_key'] = ENV['GIPHY_API_KEY']
       req.params['q'] = search_query
     end
+  end
+
+  def get_json(search_query)
+    JSON.parse(response(search_query).body)
   end
 
 end
