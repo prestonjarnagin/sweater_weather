0c181903614c0e77e988835280201d47c4aaf8f2
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-04 09:36:10 -0700

    Build out controller and gifs class

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -2,6 +2,17 @@ class Api::V1::GifsController < ApplicationController
 
   def index
 
+    location_query = params[:location]
+    location_service = GeolocationService.new(location_query)
+    coordinates = location_service.coordinates
+
+    weather = Weather.new(coordinates)
+
+    gifs = Gifs.new(weather.seven_day)
+
+
+    render json: gifs.weather
+
   end
 
 end
---
+++ app/models/gifs.rb
@@ -0,0 +1,10 @@
+class Gifs
+
+  def initialize(seven_day_weather)
+    require "pry"; binding.pry
+    @seven_day_weather = seven_day_weather
+  end
+
+  
+
+end
