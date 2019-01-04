a74f6fb0149af79fb4452fd7bda0e6be46591e8f
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-03 17:22:27 -0700

    Test sad paths

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -18,4 +18,39 @@ RSpec.describe 'User API' do
     expect(data.keys).to include(:api_key)
     expect(User.all.count).to eq(1)
   end
+
+  it 'POST /api/v1/users fails with repeated password' do
+    expect(User.all.count).to eq(0)
+
+    payload = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "different password"
+    }
+    post "/api/v1/users", params: payload
+
+    expect(response).to have_http_status(400)
+  end
+  it 'POST /api/v1/users fails with repeated email' do
+    expect(User.all.count).to eq(0)
+
+    payload = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+    post "/api/v1/users", params: payload
+
+    payload = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+    post "/api/v1/users", params: payload
+    expect(response).to have_http_status(400)
+    expect(User.all.count).to eq(1)
+
+
+
+  end
 end
