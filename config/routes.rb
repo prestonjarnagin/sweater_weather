110536e1a7a68f4891887adfb340a81493588277
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-04 09:23:23 -0700

    Open route. Create controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
       post '/users', to: 'users#create'
+
+      get '/gifs', to: 'gifs#index'
     end
   end
 end
