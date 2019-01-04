b84fae5e89a3647af1a40e30d655b3504a93d3d1
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-03 17:22:18 -0700

    Enchance test

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'User API' do
 
-  it 'POST /api/v1/users' do
+  it 'POST /api/v1/users succeeds' do
     expect(User.all.count).to eq(0)
 
-    post "/api/v1/forecast?location=#{location}"
+    payload = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+    post "/api/v1/users", params: payload
+
     expect(response).to be_successful
     data = JSON.parse(response.body, symbolize_names: true)
 
