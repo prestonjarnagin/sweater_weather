a13e75238b83e215b2ffb3db23602508c4071a56
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-06 16:31:19 -0700

    Open route for seeing favorites

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
+      get '/favorites', to: 'favorites#index'
+      get '/gifs', to: 'gifs#index'
+
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#show'
       post '/favorites', to: 'favorites#create'
 
-      get '/gifs', to: 'gifs#index'
     end
   end
 end
