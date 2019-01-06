68eb03375b4fb31659249d7dab1e88bbeed92deb
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-06 12:17:09 -0700

    Write simple test for getting user favorite cities

---
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Favorites API' do
+  describe 'GET' do
+    describe '/api/v1/favorites' do
+      it 'Succeeds' do
+        user = create(:user)
+        expect(user.favorite_cities).to be_empty
+          payload = {
+            location: 'Denver, CO',
+            api_key: user.key
+          }
+          post "/api/v1/favorites", params: payload.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+          expect(response).to be_successful
+          expect(response).to have_http_status(201)
+      end
+    end
+  end
+end
