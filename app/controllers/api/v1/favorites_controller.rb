3ade383a74ce94a3cd6d8d20959f7d9acc514f0c
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-06 16:18:18 -0700

    Update favorites controller to lookup city name wtih geolocation

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -3,7 +3,13 @@ class Api::V1::FavoritesController < ApplicationController
   def create
     begin
       user = User.find_by(key: params[:api_key])
-      user.cities.find_or_create_by(name: params[:location])
+
+      #Lookup city by geolocation
+      location = GeolocationService.new(params[:location])
+      location.state
+      city_state = "#{location.city}, #{location.state}"
+
+      user.cities.find_or_create_by(name: city_state)
       render status: 201
     rescue
       render status: 401
