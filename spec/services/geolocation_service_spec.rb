cb43ceb743cab352027ccf720fce3b3d1a3273ab
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-07 16:41:45 -0700

    Update service tests to use less verbose VCR syntax.

--- spec/services/geolocation_service_spec.rb
+++ spec/services/geolocation_service_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe GeolocationService do
+RSpec.describe GeolocationService, :vcr do
 
   let(:subject) { GeolocationService.new('denver,co') }
 
   it 'exists' do
-    VCR.use_cassette("google_geolocation_service") do
+    # VCR.use_cassette("google_geolocation_service") do
       expect(subject).to be_a(GeolocationService)
-    end
+    # end
   end
 
   it 'returns coordinates' do
-    VCR.use_cassette("google_geolocation_service") do
+    # VCR.use_cassette("google_geolocation_service") do
       expect(subject.coordinates[:lat].class).to be(Float)
       expect(subject.coordinates[:lng].class).to be(Float)
-    end
+    # end
   end
 end
--- spec/services/gif_service_spec.rb
+++ spec/services/gif_service_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe GifService do
+RSpec.describe GifService, :vcr do
 
   let(:subject) { GifService.new }
 
   it 'exists' do
-    VCR.use_cassette("gif_service") do
+    # VCR.use_cassette("gif_service") do
       expect(subject).to be_a(GifService)
-    end
+    # end
   end
 
   it 'returns a random gif' do
-    VCR.use_cassette("gif_service") do
+    # VCR.use_cassette("gif_service") do
       q = 'Cloudy'
       expect(subject.random_gif(q)).to include('giphy.com/gifs')
-    end
-
+    # end
   end
-
 end
