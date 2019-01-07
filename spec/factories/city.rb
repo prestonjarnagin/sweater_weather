a3dad5f973efd857a3ae8a47cddaf70d1632a022
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-07 11:02:13 -0700

    Change city factory

--- spec/factories/city.rb
+++ spec/factories/city.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :city do
-    sequence(:name)     { |n| "City #{n}, ST" }
+    sequence(:name)     { |n| "#{Faker::Address.city}, #{Faker::Address.state_abbr}" }
     created_at           { Time.now }
     updated_at           { Time.now }
   end
