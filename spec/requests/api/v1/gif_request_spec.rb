229b9e01732841de3eaa01cf5c2f659811b0f433
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-08 09:01:12 -0700

    Move :vcr. Remove whitespace

--- spec/requests/api/v1/gif_request_spec.rb
+++ spec/requests/api/v1/gif_request_spec.rb
@@ -1,23 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Giphy API' do
+RSpec.describe 'Giphy API', :vcr do
 
-  it 'GET /api/v1/gifs?location=denver,co', :vcr do
+  it 'GET /api/v1/gifs?location=denver,co' do
     # VCR.use_cassette("gif_feature") do
-
       location = 'denver,co'
       get "/api/v1/gifs?location=#{location}"
       expect(response).to be_successful
-
       forecasts = JSON.parse(response.body, symbolize_names: true)[:data][:daily_forecasts]
 
       expect(forecasts).to be_an_instance_of(Array)
-
       expect(forecasts).to all(be_an_instance_of(Hash))
       expect(forecasts).to all(include(:time))
       expect(forecasts).to all(include(:summary))
       expect(forecasts).to all(include(:url))
-
     # end
   end
 end
