92bf7312a0058aeffd856d4c9d9f5ca072956ab3
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-08 10:17:32 -0700

    Rewrite CitySerializer to render verbose weather data

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -3,12 +3,13 @@ class CitySerializer
 
   set_id :name
 
-  attributes :current_weather do |object|
-    # QUESTION Is this like calling a model in the view, or is it okay?
+  attributes :weather do |object|
     coordinates = GeolocationService.new(object.name).coordinates
-    # TODO The model object should have lat and lng. An api call was
-    # already made to get the name, and the lat, lng is thrown out
     weather = Weather.new(coordinates)
-    weather.current.summary
+    {
+      current: weather.current,
+      hourly: weather.hourly,
+      seven_day: weather.seven_day
+    }
   end
 end
