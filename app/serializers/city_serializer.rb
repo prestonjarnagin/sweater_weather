695b99722c0f3dabe708c63d0ffb011e7aede354
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-26 20:22:30 -0700

    Update city serializer to include only city names

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -2,14 +2,13 @@ class CitySerializer
   include FastJsonapi::ObjectSerializer
 
   set_id :name
-
-  attributes :weather do |object|
-    coordinates = GeolocationService.new(object.name).coordinates
-    weather = Weather.new(coordinates)
-    {
-      current: weather.current,
-      hourly: weather.hourly,
-      seven_day: weather.seven_day
-    }
-  end
+  # attributes :weather do |object|
+  #   coordinates = GeolocationService.new(object.name).coordinates
+  #   weather = Weather.new(coordinates)
+  #   {
+  #     current: weather.current,
+  #     hourly: weather.hourly,
+  #     seven_day: weather.seven_day
+  #   }
+  # end
 end
